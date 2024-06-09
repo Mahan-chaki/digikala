21715b3b158f725208d60cb43fb35594804aa169
Mahan-chaki <mahanch43@gmail.com>
2024-06-09 17:51:26 +0330

    tamam?

--- src/components/SendOtp.jsx
+++ src/components/SendOtp.jsx
@@ -12,23 +12,21 @@ function SendOtp({ setStep, mobile, setMobile }) {
 
   return (
     <div className={styles.container}>
-      <div>
-        <img src={logo} className={styles.img} />
-        <p className={styles.enter}>ورود | ثبت نام</p>
-        <span>سلام!</span>
-        <span>لطفا شماره موبایل خود را وارد کنید</span>
-        <input
-          type="text"
-          value={mobile}
-          maxLength={11}
-          onChange={(e) => setMobile(e.target.value)}
-        />
-        <span className={styles.span3}>لطفا این قسمت را خالی نگذارید</span>
-        <button onClick={enterHandler}>ورود</button>
-        <p className={styles.valid}>
-          ورود شما به معنای پذیرش شرایط دیجیکالا و قوانین حریم خصوصی است
-        </p>
-      </div>
+      <img src={logo} className={styles.img} />
+      <p className={styles.enter}>ورود | ثبت نام</p>
+      <span className={styles.salam}>سلام!</span>
+      <span className={styles.salam2}>لطفا شماره موبایل خود را وارد کنید</span>
+      <input
+        type="text"
+        value={mobile}
+        maxLength={11}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <span className={styles.span3}>لطفا این قسمت را خالی نگذارید</span>
+      <button onClick={enterHandler}>ورود</button>
+      <p className={styles.valid}>
+        ورود شما به معنای پذیرش شرایط دیجیکالا و قوانین حریم خصوصی است
+      </p>
     </div>
   );
 }
